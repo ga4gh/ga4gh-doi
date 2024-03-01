5eb934cf9c682706784da3b5ebc5dbe04d8ee2c3
Chen Chen <chenchen199727@gmail.com>
2024-03-01 09:18:19 -0500

    remove unused import

--- designatorGenerator.py
+++ designatorGenerator.py
@@ -1,10 +1,6 @@
 
-
-from time import monotonic
-
-
 def generateDesignator(acronym, suffix, modifier=""):
     if modifier == "":
-        return acronym + " " + suffix + "-" + modifier
-    else:
         return acronym + " " + suffix
+    else:
+        return acronym + " " + suffix + "-" + modifier
